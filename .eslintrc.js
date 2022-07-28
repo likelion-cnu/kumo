439dcf7efabe18c20840017ab48f598556337a27
최은성 <dmstjd2910000@naver.com>
2022-07-29 04:00:21 +0900

    이걸보세요

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
+  rules: {
+    quotes: [1, 'single'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'no-console': 0,
+    'no-unused-vars': 'warn',
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// eslint-disable-next-line import/no-named-as-default
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
