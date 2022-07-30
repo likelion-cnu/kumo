18387220f182c3e2aebba989b94e737d363cef73
j8won <joowoni99@gmail.com>
2022-07-30 17:09:08 +0900

    feat: 폴더, 페이지 구성 AND theme, GlobalStyle 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ThemeProvider as StyledThemeProvider,
+  createGlobalStyle,
+} from 'styled-components';
+import reset from 'styled-reset';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeCustomer from './pages/HomeCustomer';
+import styledTheme from './styles/Theme';
+
+function CustomerRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeCustomer />} />
+        <Route path="/search/:keyword" element={<HomeCustomer />} />
+        <Route path="/profile/:id" element={<HomeCustomer />} />
+        <Route path="/mystamp/:id" element={<HomeCustomer />} />
+        <Route path="/detail/:shopId" element={<HomeCustomer />} />
+        <Route path="/favorite" element={<HomeCustomer />} />
+        <Route path="/map" element={<HomeCustomer />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function AppRouter() {
+  return <CustomerRoutes />;
+}
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  * {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <StyledThemeProvider theme={styledTheme}>
+      <GlobalStyle />
+      <AppRouter />
+    </StyledThemeProvider>
   );
 }
 
---
+++ src/pages/HomeCustomer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function HomeCustomer() {
+  return <Body> </Body>;
+}
+
+const Body = styled.div``;
+
+export default HomeCustomer;
