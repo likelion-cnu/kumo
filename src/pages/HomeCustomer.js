669b90e806a4066bd328003f9a66a86b7b65c76c
j8won <joowoni99@gmail.com>
2022-07-30 20:05:38 +0900

    feat: 노토 산스 폰트 추가

--- src/pages/HomeCustomer.js
+++ src/pages/HomeCustomer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 function HomeCustomer() {
-  return <Body> </Body>;
+  return <Body>ㅇㅇㅇ</Body>;
 }
 
 const Body = styled.div``;
