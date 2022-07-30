f62b58c80992bdf9038234d7016d920b4c646cf5
j8won <joowoni99@gmail.com>
2022-07-30 22:59:10 +0900

    feat: MyQrCode - Customer Home AND Themes

--- src/pages/HomeCustomer.js
+++ src/pages/HomeCustomer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import MyQrCode from '../comonents/Homecustomer/MyQrCode';
 
 function HomeCustomer() {
-  return <Body>ㅇㅇㅇ</Body>;
+  return (
+    <Body>
+      <MyQrCode />
+    </Body>
+  );
 }
 
-const Body = styled.div``;
+const Body = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default HomeCustomer;
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,12 +1,19 @@
 export default {
   mainPurple: '#512DA8',
 
-  fontblack: '#191A22',
+  fontBlack: '#191A22',
   fontGray: '#4B4B4B',
 
   fontLight: '400',
   fontRegular: '500',
   fontBold: '600',
 
-  border: 'px solid #F5F5F5',
+  fontXSmall: '12px',
+  fontSmall: '16px',
+  fontMedium: '20px',
+  fontLarge: '24px',
+  fontXLarge: '26px',
+
+  borderGray: '1px solid #F5F5F5',
+  borderPurple: '2px solid #512DA8',
 };
