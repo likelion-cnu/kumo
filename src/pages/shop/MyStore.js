6a0c8473e1969a5eb39e6bf2e9bdb9bff5a277b4
j8won <joowoni99@gmail.com>
2022-08-17 02:02:17 +0900

    feat: Add useDaumPostcodePopup in Mystore's DetailInfo.js

--- src/pages/shop/MyStore.js
+++ src/pages/shop/MyStore.js
@@ -1,24 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Header from '../../components/shop/Header/Header';
+import DetailInfo from '../../components/shop/MyStore/DetailInfo';
 
 function MyStore() {
   return (
     <Body>
       <Header />
       <ShopDetailBox>
-        <ChangeImgBox for="input-file">
-          <input
-            type="file"
-            id="input-file"
-            onChange={e => {
-              console.log(e.target.files);
-              setImg(e.target.files[0]);
-            }}
-            style={{ display: 'none' }}
-          />
-          <ProfileImg src={img} />
-        </ChangeImgBox>
+        <DetailInfo />
       </ShopDetailBox>
     </Body>
   );
@@ -45,35 +35,6 @@ const ShopDetailBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-const ChangeImgBox = styled.label`
-  width: 40vw;
-  height: 40vw;
-  max-width: 200px;
-  max-height: 200px;
-
-  padding: 0;
-  position: relative;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  border: none;
-  border-radius: 50%;
-
-  background-color: ${props => props.theme.bgColor};
-`;
-
-const ProfileImg = styled.img`
-  width: 40vw;
-  height: 40vw;
-  max-width: 200px;
-  max-height: 200px;
-
-  object-fit: cover;
-  border-radius: 50%;
 `;
 
 export default MyStore;
